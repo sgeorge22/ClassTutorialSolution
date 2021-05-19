f9b2dbe5ceb54c9d792f96dd9212ddfd96d4d041
Sarah George <sgeorge@tql.com>
2021-05-19 16:08:57 -0400

    Add project files.

---
+++ ClassTutorial/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ClassTutorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var tqlmath = new TqlMath();
+
+            tqlmath.A = 21;
+            tqlmath.B = 30;
+
+            var sum = tqlmath.Sum();
+            Console.WriteLine($"The sum of {tqlmath.A} and {tqlmath.B} is {sum}!");
+        }
+    }
+}
---
+++ ClassTutorial/TqlMath.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassTutorial
+{
+    class TqlMath
+    {
+        public int A { get; set; }
+        public int B { get; set; }
+
+
+        public int Sum()
+        {
+            return A + B;
+        }
+    }
+}
